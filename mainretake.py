ee653862241e5e4e924fbd6ef279270ebad443cf
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-09-25 14:48:42 +0200

    Main2 is updated version

--- mainretake.py
+++ mainretake.py
@@ -1,12 +1,15 @@
 import random
 import time
 
-Game_Deck =[]
+Game_Deck = []
+Players = []
+Discards = []
+CardValue = ""
+Current_Color = ""
+Player_Number = 0
 
-Playing = True
 
 def build_UNO_Deck():
-
     colors = ["Red", "Green", "Yellow", "Blue"]
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     specials = ["Draw Two", "Skip", "Reverse"]
@@ -31,9 +34,8 @@ def build_UNO_Deck():
         for wild in wilds:
             UNO_Deck.append(wild)
 
-
-    print(UNO_Deck)
     return UNO_Deck
+
 
 def shuffle_UNO_Deck(UNO_Deck):
     for cardPos in range(len(UNO_Deck)):
@@ -41,58 +43,43 @@ def shuffle_UNO_Deck(UNO_Deck):
         UNO_Deck[cardPos], UNO_Deck[randPos] = UNO_Deck[randPos], UNO_Deck[cardPos]
     return UNO_Deck
 
-def draw_cards(NumCards, GameDeck):
 
+def draw_cards(NumCards, GameDeck):
     Drawn_Cards = []
 
     for i in range(NumCards):
-        Drawn_Cards.append(Game_Deck.pop(0))
+        Drawn_Cards.append(GameDeck.pop(0))
     return Drawn_Cards
 
-def number_of_players(Num_Players, Game_Deck):
 
+def number_of_players(Num_Players, GameDeck):
     for player in range(Num_Players):
-        Players.append(draw_cards(7, Game_Deck))
+        Players.append(draw_cards(7, GameDeck))
 
-    return Players, Game_Deck
 
-def current_hand(Current_Player, Players_Hand):
+def current_hand(Current_Player):
     print("Player {}".format(Current_Player + 1), "is now playing.")
     print("Your Current Hand:")
     print("......................")
     i = 1
-    for card in Players_Hand:
+    for card in Players[Current_Player]:
         print(i, card)
-        i = i+1
+        i = i + 1
     print(" ")
 
-    return
 
 def valid_card(color, value, Players_Hand):
-
     for card in Players_Hand:
-
         if "Wild" in card:
             return True
-
         elif color in card or value in card:
             return True
-
     return False
 
 
 def start_game():
-    global Game_Deck
-    global Discards
-    global CardValue
-    global Current_Color
-    global Player_Number
-    global Players
-
-    Players = []
-
+    global Game_Deck, Discards, CardValue, Current_Color, Player_Number, Players
     Discards = []
-
     Game_Deck = build_UNO_Deck()
     Game_Deck = shuffle_UNO_Deck(Game_Deck)
 
@@ -101,52 +88,43 @@ def start_game():
     while Player_Number < 2 or Player_Number > 4:
         Player_Number = int(input("ERROR: Number of players invalid, please give a value of 2, up until 4: "))
 
-    Players, Game_Deck = number_of_players(Player_Number, Game_Deck)
+    number_of_players(Player_Number, Game_Deck)
 
-    Discards.append(Game_Deck.pop(0))
-    splitCard = Discards[0].split(" ", 1)
-    Current_Color = splitCard[0]
+    top_card = Game_Deck.pop(0)
+    Discards.append(top_card)
+    split_card = top_card.split(" ", 1)
+    Current_Color = split_card[0]
 
     if Current_Color != "Wild":
-        CardValue = splitCard[1]
-
+        CardValue = split_card[1]
     else:
         CardValue = "Any"
 
 
-while Playing:
-    Players = []
+start_game()
 
-    PlayerTurn = 0
+PlayerTurn = 0  # Initialize player turn outside the loop
 
-    current_hand(PlayerTurn, Players[PlayerTurn])
-
+while True:
+    current_hand(PlayerTurn)
     print("Top of pile: {}".format(Discards[-1]))
 
     if valid_card(Current_Color, CardValue, Players[PlayerTurn]):
         ChosenCard = int(input("Please select a card to play: "))
-        while not valid_card(Current_Color, CardValue, [Players[PlayerTurn][ChosenCard - 1]]):
+        while not (1 <= ChosenCard <= len(Players[PlayerTurn])) or \
+                not valid_card(Current_Color, CardValue, [Players[PlayerTurn][ChosenCard - 1]]):
             ChosenCard = int(input("Please choose a valid card to play: "))
 
         print("You have played {}".format(Players[PlayerTurn][ChosenCard - 1]))
-
         Discards.append(Players[PlayerTurn].pop(ChosenCard - 1))
-
     else:
-        print("No cards available are valid to play, please pick up from pile.")
+        print("No cards available are valid to play, please pick up from the pile.")
         Players[PlayerTurn].extend(draw_cards(1, Game_Deck))
-    print(" ")
 
+    print(" ")
     PlayerTurn += 1
 
     if PlayerTurn == Player_Number:
         PlayerTurn = 0
-
     elif PlayerTurn < 0:
         PlayerTurn = Player_Number - 1
-
-
-
-Current_Player = 1
-
-start_game()
