2ed5c9593da4aae6481284e9e2ade3ae3189f378
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-11-03 10:50:20 +0100

    Update mainretakerefactored.py

--- mainretakerefactored.py
+++ mainretakerefactored.py
@@ -1,80 +1,153 @@
 import random
 
-
-class Card:
-    def __init__(self, color, value):
-        self.colors = color
-        self.value = value
-
-
-class Player:
-    def __init__(self, id):
-        self.id = id
-        self.hand = []
-
-    def draw(self, deck, num=1):
-        for _ in range(num):
-            self.hand.append(deck.pop_card())
-
-    def play_card(self, index):
-        return self.hand.pop(index)
-
-
-class UnoGame:
+class UNOGame:
     def __init__(self, num_players):
         self.num_players = num_players
-        self.deck = self.build_deck()
-        self.players = self.init_players()
-        self.current_player_index = 0
+        self.colors = ["Red", "Green", "Yellow", "Blue"]
+        self.numbers = [str(i) for i in range(10)]
+        self.specials = ["Draw Two", "Skip", "Reverse"]
+        self.wilds = ["Wild Card", "Wild Draw Four"]
+        self.players = [[] for _ in range(num_players)]
+        self.current_player = 0
         self.direction = 1
-        self.discards = []
-        random.shuffle(self.deck)
+        self.game_deck, self.top_card, self.split_card = self.build_and_shuffle_deck()
+        self.discards = [self.top_card]
+        self.current_color, self.card_value = self.split_card
 
-    def build_deck(self):
-        colors = ["Red", "Green", "Yellow", "Blue"]
-        numbers = list(range(10)) + ["Draw Two", "Skip", "Reverse"]
-        wilds = ["Wild Card", "Wild Draw Four"]
-        return [Card(color, value) for color in colors for value in numbers] \
-            + [Card("", wild) for wild in wilds for _ in range(4)]
+    def build_and_shuffle_deck(self):
+        game_deck = []
 
-    def pop_card(self):
-        return self.deck.pop()
+        for color in self.colors:
+            for number in self.numbers:
+                for _ in range(2):
+                    if number == "0":
+                        card = f"{color} {number}"
+                    else:
+                        card = f"{color} {number}"
+                    game_deck.append(card)
 
-    def init_players(self):
-        return [Player(i) for i in range(self.num_players)]
+            for special in self.specials:
+                card = f"{color} {special}"
+                game_deck.extend([card] * 2)
 
-    def play(self):
-        # draw initial card
-        self.discards.append(self.pop_card())
-        # game loop
+        for _ in range(4):
+            game_deck.extend(self.wilds)
+
+        random.shuffle(game_deck)
+        top_card = game_deck.pop(0)
+
+        while top_card.startswith("Wild") or top_card.split()[1] in self.specials:
+            random.shuffle(game_deck)
+            top_card = game_deck.pop(0)
+
+        split_card = top_card.split()
+        return game_deck, top_card, split_card
+
+    def draw_cards(self, num_cards):
+        drawn_cards = []
+        for _ in range(num_cards):
+            drawn_cards.append(self.game_deck.pop(0))
+        return drawn_cards
+
+    def valid_card(self, color, value, player_hand):
+        for card in player_hand:
+            card_parts = card.split()
+            if "Wild" in card or (len(card_parts) == 2 and (color == card_parts[0] or value == card_parts[1])):
+                return True
+        return False
+
+    def current_hand(self):
+        print(f"Player {self.current_player + 1} is now playing.")
+        print("Your Current Hand:")
+        print("......................")
+        for i, card in enumerate(self.players[self.current_player], start=1):
+            print(i, card)
+        print(" ")
+
+    def start_game(self):
+        while self.num_players < 2 or self.num_players > 4:
+            try:
+                self.num_players = int(input("How Many People are playing: "))
+                if not (2 <= self.num_players <= 4):
+                    print("ERROR: Number of players invalid, please give a value of 2, up to 4.")
+            except ValueError:
+                print("ERROR: Please enter a valid integer.")
+
+        self.number_of_players()
+
+    def number_of_players(self):
+        for player in range(self.num_players):
+            self.players[player] = self.draw_cards(7)
+
+    def play_turn(self):
+        self.current_hand()
+        print("Top of pile:", self.discards[-1])
+
+        if self.valid_card(self.current_color, self.card_value, self.players[self.current_player]):
+            chosen_card = int(input("Please select a card to play: ")) - 1
+
+            while not (0 <= chosen_card < len(self.players[self.current_player])) or not self.valid_card(
+                    self.current_color, self.card_value, [self.players[self.current_player][chosen_card]]):
+                chosen_card = int(input("Please choose a valid card to play: ")) - 1
+
+            print("You have played", self.players[self.current_player][chosen_card])
+            self.discards.append(self.players[self.current_player].pop(chosen_card))
+        else:
+            print(f"Player {self.current_player + 1} No cards available are valid to play, please pick up from the pile.")
+            self.players[self.current_player].extend(self.draw_cards(1))
+
+        split_card = self.discards[-1].split()
+        self.current_color, self.card_value = split_card
+
+        if self.current_color == "Wild":
+            for i, color in enumerate(self.colors, start=1):
+                print(f"{i} {color}")
+            color_update = int(input("What color would you like to change to: ")) - 1
+
+            while color_update < 0 or color_update >= len(self.colors):
+                color_update = int(input("Invalid Option Given. What color would you like to change to: ")) - 1
+
+            self.current_color = self.colors[color_update]
+            print("Colour has been changed to:", self.current_color)
+
+            if self.card_value == "Draw Four":
+                next_player = (self.current_player + self.direction) % self.num_players
+                print(f"Player {next_player + 1} is drawing 4 more cards.")
+                self.players[next_player].extend(self.draw_cards(4))
+
+        if self.card_value == "Reverse":
+            print("Direction of play has been reversed.")
+            self.direction *= -1
+
+        if self.card_value == "Skip":
+            print("Next Player has been skipped.")
+            self.current_player = (self.current_player + self.direction) % self.num_players
+
+        if self.card_value == "Draw Two":
+            next_player = (self.current_player + self.direction) % self.num_players
+            print(f"Player {next_player + 1} is drawing 2 more cards.")
+            self.players[next_player].extend(self.draw_cards(2))
+
+        self.current_player = (self.current_player + self.direction) % self.num_players
+
+        if self.current_player < 0:
+            self.current_player = self.num_players - 1
+
+    def play_game(self):
         while True:
-            current_player = self.players[self.current_player_index]
-            print("Current Player:", current_player.id)
-            print("Top card:", self.discards[-1].colors, self.discards[-1].value)
-            valid_card_indices = [i for i, card in enumerate(current_player.hand)
-                                  if self.is_valid_play(card)]
-            if valid_card_indices:
-                # player chooses a valid card
-                card_index = self.choose_card(valid_card_indices)
-                self.discards.append(current_player.play_card(card_index))
-                print("Player played:", self.discards[-1].colors, self.discards[-1].value)
-            else:
-                # player must draw a card
-                current_player.draw(self)
-            self.next_player()
-
-    def is_valid_play(self, card):
-        top_discard = self.discards[-1]
-        return card.colors == top_discard.colors or card.value == top_discard.value
-
-    def choose_card(self, valid_card_indices):
-        return valid_card_indices[0]
-
-    def next_player(self):
-        def next_player(self):
-            self.current_player = (self.current_player + 2) % len(self.players)
+            self.play_turn()
 
 
 if __name__ == "__main__":
-    game = UnoGame(4)
-    game.play()
+    num_players = None
+    while num_players is None:
+        try:
+            num_players = int(input("How Many People are playing: "))
+            if not (2 <= num_players <= 4):
+                print("ERROR: Number of players invalid, please give a value of 2, up until 4.")
+                num_players = None
+        except ValueError:
+            print("ERROR: Please enter a valid integer.")
+
+    uno_game = UNOGame(num_players)
+    uno_game.play_game()
